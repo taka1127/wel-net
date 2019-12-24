64027ce7a36c641d498fd1d7105a52154d8319e7
taka1127 <taka07251127@gmail.com>
2019-12-24 20:57:33 +0900

    edit-4

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,11 +1,21 @@
 class InformationsController < ApplicationController
-
   def index
     @informations = Information.all
   end
 
   def new
-    @informations = Information.new
+    @information = Information.new
+  end
+
+  def create
+    @infomation = Information.new(information_params)
+    @infomation.save
+    # redirect_to root_path
+  end
+
+  private
+  def information_params
+    params.permit(:name, :image, :text)
   end
   
 end
