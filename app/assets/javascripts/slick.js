29eb0c0422baae8d960d319f02fe5d5bbb018dad
taka1127 <taka07251127@gmail.com>
2019-12-24 11:54:54 +0900

    edit-3

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -1,6 +1,9 @@
 $(function(){
   $("#slider").slick({
-    prevArrow: '<img src="./images/6172f59d8a251947e60f10bcee55000b_w.jpg" class="slide-arrow prev-arrow">',
-    nextArrow: '<img src="./assets/images/AS20190719000785_commL.jpg" class="slide-arrow next-arrow">'
+    arrows: true,
+    slidesToShow:3,
+    centerMode:true,
+    prevArrow:'<div class="prev">PREV</div>',
+    nextArrow:'<div class="next">NEXT</div>' 
   });
 });
