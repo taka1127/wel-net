8df22ff82d4637f246e964c006c15f5391db52c6
taka1127 <taka07251127@gmail.com>
2019-12-26 14:56:20 +0900

    edit-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   #deviseを使ったofficeテーブル機能
-  devise_for :offices,controllers: {
-    sessions:      'admins/sessions',
-    passwords:     'admins/passwords',
-    registrations: 'admins/registrations'
-  }   
+  devise_for :offices
+  # devise_for :offices,controllers: {
+  #   sessions:      'offices_sessions_path',
+  #   passwords:     'offices_passwords_path',
+  #   registrations: 'offices_registrations_path'
+  # }   
   #deviseを使ったuserテーブル機能
-  devise_for :users,controllers: {
-    sessions:      'users/sessions',
-    passwords:     'users/passwords',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
+  # devise_for :users,controllers: {
+  #   sessions:      'users_sessions_path',
+  #   passwords:     'users_passwords_path',
+  #   registrations: 'users_registrations_path'
+  # }
   root "informations#index"
   resources :informations #各アクションの定義
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_025839) do
+ActiveRecord::Schema.define(version: 2019_12_26_052337) do
 
   create_table "information", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_025839) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "office_name"
     t.index ["email"], name: "index_offices_on_email", unique: true
     t.index ["reset_password_token"], name: "index_offices_on_reset_password_token", unique: true
   end
@@ -43,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_025839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "office_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
