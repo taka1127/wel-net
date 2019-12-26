ce50e7196b8eee088357605ab5f9a2f6bde6be4d
taka1127 <taka07251127@gmail.com>
2019-12-26 13:44:21 +0900

    edit-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users     #deviseを使ったユーザー機能
+  #deviseを使ったofficeテーブル機能
+  devise_for :offices,controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }   
+  #deviseを使ったuserテーブル機能
+  devise_for :users,controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
   root "informations#index"
   resources :informations #各アクションの定義
 end
