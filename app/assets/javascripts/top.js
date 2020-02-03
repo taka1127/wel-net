97e6a6ccddd104f744df6d45b0ddf701f037753d
taka1127 <taka07251127@gmail.com>
2020-02-03 17:51:20 +0900

    トップページ仮マージ

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,4 +1,17 @@
 $(function(){
   // console.log('hello');
-  $('p').hide().fadeIn(3000);
+  $('#text').hide().fadeIn(3000);
+
+  $('#text').hover(
+    function() {
+      // 子要素の「.text-contents」の要素を取得し、text-activeクラスをつけてください
+      $(this).find('#contents').addClass('text-active');
+      
+    },
+    function() {
+      // 子要素の「.text-contents」の要素を取得し、text-activeクラスを外してください
+      $(this).find('#contents').removeClass('text-active');
+      
+    }
+  );
 });
