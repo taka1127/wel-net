43bd343696f547a2645aaf0ffbdf6411922377c5
taka1127 <taka07251127@gmail.com>
2020-02-05 00:29:05 +0900

    リージョン修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1' 
     }
     config.fog_directory  = 'welnet' #S3のバケット名
-    config.asset_host = 'https://s3-your_region.amazonaws.com/welnet'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/welnet'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
