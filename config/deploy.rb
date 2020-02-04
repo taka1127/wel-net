36fcd34e82e4cee4f367ac4ffd6f24d2d4415b8b
taka1127 <taka07251127@gmail.com>
2020-02-04 15:31:03 +0900

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,4 +33,16 @@ namespace :deploy do
     invoke 'unicorn:stop'
     invoke 'unicorn:start'
   end
+
+  desc 'upload credentials.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/credentials.yml', "#{shared_path}/config/credentials.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
