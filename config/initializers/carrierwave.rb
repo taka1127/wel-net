1409bb568318d609a74fc5f04869286f8cd9283d
taka1127 <taka07251127@gmail.com>
2020-02-05 00:22:01 +0900

    本番環境のみS3保存

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'welnet'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/welnet'
+  if Rails.env.production? # 本番環境:AWS使用
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1' 
+    }
+    config.fog_directory  = 'welnet' #S3のバケット名
+    config.asset_host = 'https://s3-your_region.amazonaws.com/welnet'
+  else
+    config.storage :file # 開発環境:public/uploades下に保存
+    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+  end  
 end
